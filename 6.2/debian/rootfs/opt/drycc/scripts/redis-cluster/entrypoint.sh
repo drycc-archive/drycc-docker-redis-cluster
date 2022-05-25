008b715c9e877c888c86d95fa8b8af8d3c4d5998
jianxiaoguo <842666271@qq.com>
2022-05-25 15:02:53 +0800

    feat(drycc-docker-redis-cluster): build redis-cluster-6.2 image

---
+++ 6.2/debian/rootfs/opt/drycc/scripts/redis-cluster/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purpose
+
+# Load Redis environment variables
+. /opt/drycc/scripts/redis-cluster-env.sh
+
+# Load libraries
+. /opt/drycc/scripts/libbitnami.sh
+. /opt/drycc/scripts/librediscluster.sh
+
+# print_welcome_page
+
+if [[ "$*" = *"/run.sh"* ]]; then
+    info "** Starting Redis setup **"
+    /opt/drycc/scripts/redis-cluster/setup.sh
+    info "** Redis setup finished! **"
+fi
+
+echo ""
+exec "$@"
